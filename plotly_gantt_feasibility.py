a958d69df774aa8d3de13ff5efcd719833203b9a
alannaz36 <amzosc001@gmail.com>
2021-04-16 19:38:15 +0000

    Testing feasibility of using plotly gantt - feasible to create desired plot, but need to display small sections of the data at a time

--- plotly_gantt_feasibility.py
+++ plotly_gantt_feasibility.py
@@ -1,11 +1,9 @@
 import plotly.offline as po
 import plotly.graph_objs as go
 
-import numpy # <-- required for figure_factory
-import plotly.figure_factory as ff # gantt
 import pandas as pd # data
 import sqlite3      # data
-#import plotly.express as px <-- must come after pandas
+import plotly.express as px #<-- must come after pandas
  
 # pip install numpy
 # pip install pandas
@@ -32,7 +30,29 @@ def show_qt(fig): # <-- treated as another button
     fig_view.raise_()
     return fig_view
  
- 
+
+# This overwrite method was obtained from: https://stackoverflow.com/questions/66078893/plotly-express-timeline-for-gantt-chart-with-integer-xaxis
+def integer_process_dataframe_timeline(args):
+    """
+    Overwrite conversion to datetime input for px.timeline()
+    """
+    print("Processing x-axis data as an integer rather than a datetime")
+    args["is_timeline"] = True
+    if args["x_start"] is None or args["x_end"] is None:
+        raise ValueError("Both x_start and x_end are required")
+    
+    x_start = args["data_frame"][args["x_start"]]
+    x_end = args["data_frame"][args["x_end"]]
+    
+    # No columns added to the dataframe, no risk of overwrite
+    args["data_frame"][args["x_end"]] = (x_end - x_start)
+    args["x"] = args["x_end"]
+    del args["x_end"]
+    args["base"] = args["x_start"]
+    del args["x_start"]
+    return args
+
+
 if __name__ == '__main__':
     app = QtWidgets.QApplication(sys.argv)
     
@@ -42,10 +62,13 @@ if __name__ == '__main__':
     name_connection = sqlite3.connect("metrics/plugin_name.sqlite")
     plugin_name_sql = 'SELECT * FROM plugin_name'
     plugin_name_df = pd.read_sql(plugin_name_sql, con=name_connection, index_col="plugin_id")
+    name_connection.close()
     # plugin_id and plugin_name columns
     
     # Read plugin run_time data from database
-    connection = sqlite3.connect("metrics/switchboard_callback.sqlite")
+    #  switchboard_callback.sqlite with table switchboard_callback
+    #  threadloop_iteration.sqlite with table threadloop_iteration
+    data_connection = sqlite3.connect("metrics/switchboard_callback.sqlite")
     sql = ('SELECT ' +
         'plugin_id, ' +
         'cpu_time_start, ' +
@@ -53,17 +76,21 @@ if __name__ == '__main__':
         'FROM ' +
         'switchboard_callback'
     )
-    df = pd.read_sql(sql, con=connection)
+    df = pd.read_sql(sql, con=data_connection)
+    data_connection.close()
     
     # Replace plugin_id with plugin_name
     df = df.rename(columns={'plugin_id': 'plugin_name'}, errors="raise")
     df = df.replace(to_replace=plugin_name_df.to_dict())
     
-    # SORT BY cpu_time_start    
-    df = df.rename(columns={'plugin_name': 'Task', 'cpu_time_start': 'Start', 'cpu_time_stop': 'Finish'}, errors="raise")
+    # Sort by cpu_time_start   
+    df = df.sort_values(by=['cpu_time_start'])
     
-    fig = ff.create_gantt(df.head(n=50), title=None, group_tasks=True, index_col='Task', show_colorbar=True)
-    fig.layout.xaxis.rangeselector = None
+    # Specify custom dataframe processor method to avoid plotly express' auto-use of datetime objects
+    px._core.process_dataframe_timeline = integer_process_dataframe_timeline
+    fig = px.timeline(df.head(n=5), x_start="cpu_time_start", x_end="cpu_time_stop", y="plugin_name", color="plugin_name", labels={'plugin_name': 'Plugin Name', 'cpu_time_start': 'Start Time (ns)', 'cpu_time_stop': 'End Time (ns)'}) 
+    fig.layout.xaxis.type = 'linear'
+    fig.layout.xaxis.title = 'Time (ns)'
     fig_view = show_qt(fig) 
     
     sys.exit(app.exec_())
